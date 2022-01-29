eeda74bca3848aa360c05fea4f3385f8abef44e7
Bruno Gião <66087393+greybrunix@users.noreply.github.com>
2022-01-29 21:28:48 +0000

    50 questions
    I feel confident in the other exercises for now, will finish when needed

--- 50q.hs
+++ 50q.hs
@@ -10,10 +10,10 @@ enumFromThenTo' start next end
     | otherwise = start:enumFromThenTo' next (2 * next - start) end
     -- this one is too scary sorry
 
-concat' :: [a] -> [a] -> [a]
-concat' [] l = l
-concat' l [] = l
-concat' (x:xs) (h:t) = x:(concat' xs (h:t))
+plusplus :: [a] -> [a] -> [a]
+plusplus [] l = l
+plusplus l [] = l
+plusplus (x:xs) (h:t) = x:(plusplus xs (h:t))
 
 indexElem :: [a] -> Int -> a
 indexElem [] _ = error"This position does not exist"
@@ -32,3 +32,50 @@ drop' :: Int -> [a] -> [a]
 drop' _ [] = []
 drop' 0 l = l
 drop' n (x:xs) = drop' (n-1) xs
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (h:t) (a:b) = (h,a):(zip t b)
+
+replicate' :: Int -> a -> [a]
+replicate' n r = if n > 0 then r:(replicate' (n-1) r) else []
+
+intersperse' :: a -> [a] -> [a]
+intersperse' _ [] = []
+intersperse' _ [x] = [x]
+intersperse' n (x:xs) = x:([n]++intersperse' n xs)
+
+group' :: Eq a => [a] -> [[a]]
+group' [] = []
+group' [x] = [[x]]
+group' (x:xs) | x == head (head lr) = (x: head lr) : (tail lr) -- note on this head of head further down (1)
+              | otherwise = [x] : (head lr) : (tail lr)
+              where lr = group' xs
+-- (1) this is a list of lists so the head of the list of lists is a list, we can't compare an element to a list
+
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' [[x]] = [x]
+concat' (x:xs) = (aux [] x) ++ concat' xs
+                where aux acc []     = acc
+                      aux acc (h:[]) = acc ++ [h]
+                      aux acc (h:t)  = aux (acc ++ [h]) t
+delete' :: Eq a => a -> [a] -> [a]
+delete' _ [] = []
+delete' x (h:t)
+    | x == h = t
+    | otherwise = h:delete' x t
+
+remove :: Eq a => [a] -> [a] -> [a]
+remove l [] = l
+remove [] _ = []
+remove l (h:t) = remove (delete' h l) t
+
+pGreater :: Ord a => [a] -> Int
+pGreater [_] = 0
+pGreater (h:t) | h > ((!!) t x) = 0 -- (2) 
+               | otherwise = 1 + x
+               where x = pGreater t
+               -- (2) x is the index of the greatest element in t so the bang bang function will get hte greatest elem in t
+         
